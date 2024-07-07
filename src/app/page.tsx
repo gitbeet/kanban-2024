9adc8e4030aa88e4aa38673e19a077c3a4b7092e
gitbeet <nikig715@gmail.com>
2024-07-07 18:19:00 +0200

    Basic schema (board, col, task)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,44 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const boards = await db.query.boards.findMany({
+    with: {
+      columns: {
+        with: {
+          tasks: true,
+        },
+      },
+    },
+  });
+  console.log(boards);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <section className="flex gap-32">
+        {boards.map((board) => (
+          <div key={board.id}>
+            <h2 className="pb-4 text-xl">{board.name}</h2>
+            <div className="flex gap-16">
+              {board.columns.map((col) => (
+                <div key={col.id}>
+                  <p className="border-b p-2">{col.name}</p>
+                  <div>
+                    {col.tasks.map((task) => (
+                      <p key={task.id}>
+                        <span className="pr-2">
+                          {task.completed ? "v" : "x"}
+                        </span>
+                        {task.name}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,93 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   index,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
+  integer,
+  boolean,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `kanban-2024_${name}`);
 
-export const posts = createTable(
-  "post",
+// ---------- MODELS ----------
+
+export const boards = createTable(
+  "board",
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    id: varchar("id", { length: 1024 }).primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    userId: varchar("userId", { length: 256 }).notNull(),
+
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+    nameIndex: index("board_name_idx").on(example.name),
+  }),
 );
+
+export const columns = createTable(
+  "column",
+  {
+    id: varchar("id", { length: 1024 }).primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    boardId: varchar("boardId", { length: 1024 }).notNull(),
+
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (example) => ({
+    nameIndex: index("column_name_idx").on(example.name),
+  }),
+);
+
+export const tasks = createTable(
+  "task",
+  {
+    id: varchar("id", { length: 1024 }).primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    columnId: varchar("columnId", { length: 1024 }).notNull(),
+    completed: boolean("completed").notNull().default(false),
+
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (example) => ({
+    nameIndex: index("task_name_idx").on(example.name),
+  }),
+);
+
+//---------- RELATIONS ----------
+
+export const boardRelations = relations(boards, ({ many }) => ({
+  columns: many(columns),
+}));
+
+export const columnsRelations = relations(columns, ({ one, many }) => ({
+  board: one(boards, {
+    fields: [columns.boardId],
+    references: [boards.id],
+  }),
+  tasks: many(tasks),
+}));
+
+export const tasksRelations = relations(tasks, ({ one }) => ({
+  column: one(columns, {
+    fields: [tasks.columnId],
+    references: [columns.id],
+  }),
+}));
