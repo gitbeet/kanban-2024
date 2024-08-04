24631e7edbadafe7c9264466ad379b405bb605f9
gitbeet <nikig715@gmail.com>
2024-08-04 19:00:39 +0200

    clerk theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { type Metadata } from "next";
 import { getBoards } from "~/server/queries";
 import Menus from "~/components/menus/menus";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { dark, neobrutalism, shadesOfPurple } from "@clerk/themes";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   weight: ["400", "500", "700"],
@@ -27,11 +28,15 @@ export default async function RootLayout({
   const boards = await getBoards();
   // await saveEditTaskChanges();
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+      }}
+    >
       <UIProvider>
         <BoardsProvider boards={boards}>
           <html lang="en" className={`${plusJakartaSans.variable}`}>
-            <body className="bg-neutral-850 mx-auto flex h-[100dvh] flex-col text-white">
+            <body className="mx-auto flex h-[100dvh] flex-col bg-neutral-850 text-white">
               <TopNav />
               {children}
               <Menus />
--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -3,7 +3,6 @@
 import { useUI } from "~/context/ui-context";
 import { useBoards } from "~/context/boards-context";
 import { SignedIn, UserButton } from "@clerk/nextjs";
-
 const TopNav = () => {
   const { getCurrentBoard, optimisticBoards } = useBoards();
   const { setShowSidebar } = useUI();
@@ -12,7 +11,7 @@ const TopNav = () => {
   const noBoards = !optimisticBoards.length;
 
   return (
-    <nav className="border-neutral-750 relative z-20 flex h-fit items-center justify-between border-b bg-neutral-800 px-4 py-6 shadow-md">
+    <nav className="relative z-20 flex h-fit items-center justify-between border-b border-neutral-750 bg-neutral-800 px-4 py-6 shadow-md">
       <SignedIn>
         <h1 className="text-2xl font-bold">kanban</h1>
 
