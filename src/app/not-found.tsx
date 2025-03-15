42802aa668037551e4a58ab894d510bc5dde0e77
gitbeet <nikig715@gmail.com>
2025-03-15 13:40:18 +0100

    basic not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <section className="">
+      <h1 className="text-heading text-dark text-center">Page not found</h1>
+    </section>
+  );
+};
+
+export default NotFound;
