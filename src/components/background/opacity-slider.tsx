e3596a46e97a85825da1c75ed1aae58b670e6622
gitbeet <nikig715@gmail.com>
2025-03-29 19:58:07 +0100

    use-debounce, opacity slider modifyUserData with debounce

--- src/components/background/opacity-slider.tsx
+++ src/components/background/opacity-slider.tsx
@@ -1,7 +1,35 @@
 import { useBackground } from "~/context/bg-context";
+import { useDebouncedCallback } from "use-debounce";
+import { useEffect } from "react";
+import { modifyUserData } from "~/server/queries";
+import { showCustomErrorToast } from "~/utilities/showCustomErrorToast";
+
+const DEBOUNCE_DELAY = 1500;
 
 const OpacitySlider = ({ tabIndex = 1 }: { tabIndex?: number }) => {
   const { imageOpacity, setImageOpacity } = useBackground();
+  const handleChangeDbOpacity = useDebouncedCallback(
+    async (newOpacity: number) => {
+      const result = await modifyUserData({ backgroundOpacity: newOpacity });
+      if (result?.error) showCustomErrorToast({ message: result.error });
+    },
+    DEBOUNCE_DELAY,
+  );
+
+  const changeOpacity = async () => {
+    try {
+      await handleChangeDbOpacity(imageOpacity);
+    } catch (error) {
+      showCustomErrorToast({ message: "Client error" });
+    }
+  };
+
+  useEffect(() => {
+    changeOpacity()
+      .then()
+      .catch(() => showCustomErrorToast({ message: "Client error" }));
+  }, [imageOpacity]);
+
   return (
     <div className="space-y-2">
       <h4>Opacity</h4>
