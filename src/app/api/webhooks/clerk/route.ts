c40d4190018ce1d647866f6c1b38b127d5b00a3b
gitbeet <nikig715@gmail.com>
2025-03-29 12:07:26 +0100

    type error fix

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,32 +1,13 @@
-// import { Webhook } from 'svix';
-import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
-import { getOrCreateUserData } from "~/server/queries";
+import { type WebhookEvent } from "@clerk/nextjs/server";
+import { type NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  //   const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-
-  //   if (!WEBHOOK_SECRET) {
-  //     throw new Error("CLERK_WEBHOOK_SECRET is not set");
-  //   }
-
-  const payload: WebhookEvent = await req.json();
+export async function POST(req: NextRequest) {
+  const payload = (await req.json()) as WebhookEvent;
   console.log(payload);
 
-  // Handle user creation event
-  //   if (evt.type === 'user.created') {
-  //     const { id } = evt.data;
-  //     try {
-  //       await getOrCreateUserData(id);
-  //     } catch (error) {
-  //       console.error('Error creating user data:', error);
-  //     }
-  //   }
-
-  //   return new Response('', { status: 200 });
-  return Response.json({ message: "Received" });
+  return NextResponse.json({ message: "Received" });
 }
 
 export async function GET() {
-  return Response.json({ message: "Hello World!" });
+  return NextResponse.json({ message: "Hello World!" });
 }
